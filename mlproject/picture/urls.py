589a8cbb9e9409f8e2e5b1c8dae3ebb030dcf751
evankar <csd3774@csd.uoc.gr>
2018-06-20 23:43:37 +0300

    Change Bootstrap

--- mlproject/picture/urls.py
+++ mlproject/picture/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.HomePageView.as_view(), name='home'),
+	path('about/', views.AboutPageView.as_view(), name='about'),
+#	path('features/', views.FeaturesPageView.as_view(), name='features')
 ]
- 
--- mlproject/picture/views.py
+++ mlproject/picture/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def index(request):
-	return HttpResponse("<h2>HEY!</h2>")
+class HomePageView(TemplateView):
+	template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+	template_name = 'about.html'
